5e1187a819bd6fcc5f862c3e14630e1bb34e6b0c
alexanto <szanto.szandra@gmail.com>
2021-02-09 09:33:24 +0100

    feat: add error handler

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const requestLogger = require('./middleware/logger');
+const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
 
 app.use(requestLogger);
+app.use(errorHandler);
 
 module.exports = app;
