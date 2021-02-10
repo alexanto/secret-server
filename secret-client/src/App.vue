b53f027c0fe2dee29f88bb79f8e52628eb974da9
alexanto <szanto.szandra@gmail.com>
2021-02-10 22:25:02 +0100

    feat: add skeleton for client

---
+++ secret-client/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <SecretClient msg="Secret Manager"/>
+  </div>
+</template>
+
+<script>
+import SecretClient from './components/SecretClient.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SecretClient
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
